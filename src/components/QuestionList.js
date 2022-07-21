c48c1fb482d74d0c684dbd5c6c1430c3583ec8de
AmosJune <amos.sifuma@student.moringaschool.com.com>
2022-07-22 01:08:55 +0300

    Finished

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+//Fetch all the questions 
+  useEffect(() => {
+    fetch(`http://localhost:4000/questions`)
+      .then((resp) => resp.json())
+      .then((questions) => {
+        setQuestions(questions)
+      });
+  }, []);
+
+// To handle a deletion click
+function handleDeleteClick(id) {
+  fetch(`http://localhost:4000/questions/${id}`, {
+    method: "DELETE",
+  })
+    .then((resp) => resp.json)
+    .then(() => {
+      const updatedQuestions = questions.filter((quiz) => 
+          quiz.id !== id);
+      setQuestions(updatedQuestions)
+    });
+};
+
+//Handle an answer
+  function handleAnswerChange(correctIndex, id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type" : "application/json",
+      },
+      body: JSON.stringify({ correctIndex }),
+    })
+      .then((resp) => resp.json())
+      .then((updatedQuestionList) => {
+        const updatedQuestions = questions.map((quiz) => {
+          if (quiz.id === updatedQuestionList.id){
+            return updatedQuestionList
+          }
+          else {
+            return quiz;
+          }
+        });
+        setQuestions(updatedQuestions)
+      });
+  }
+
+  const questionItems = questions.map((quiz) => (
+    <QuestionItem 
+      key={quiz.id} onAnswerChange={handleAnswerChange}
+      onDeleteClick={handleDeleteClick} question={quiz} />
+  ));
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{/* display QuestionItem components here after fetching */}
+        {questionItems}
+      </ul>
     </section>
   );
 }
